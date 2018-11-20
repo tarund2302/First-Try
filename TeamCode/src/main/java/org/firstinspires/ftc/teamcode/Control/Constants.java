6f41a9ad0f155d35b3c2a766c8da1b375ed6b0b8
tarund2302 <tarund2302@gmail.com>
2018-11-19 19:11:51 -0500

    changed servo positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Constants.java
@@ -14,10 +14,12 @@ public interface Constants {
     double NEVEREST_40_COUNTS_PER_REV = 1120; //drivetrain motor ticks
     double ORBITAL_3_7_COUNTS_PER_REV = 103; //actuator motor ticks
 
+
     double WHEEL_DIAMETER_INCHES = 4.0;
     double DRIVE_GEAR_REDUCTION = 0.5; //gear ratio (driven gear / driving gear)
     double COUNTS_PER_INCH = (NEVEREST_40_COUNTS_PER_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
+    //used for encoder drive
     double DRIVE_SPEED = 0.5;
     double TURN_SPEED = 0.2;
 
