3eca98b462b93a599b893d202526f57923b69250
tarund2302 <tarund2302@gmail.com>
2018-11-25 00:09:23 -0500

    made PID Controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/PIDController.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Control;
+
+public class PIDController implements Constants{
+    private double i=0;
+    private double d;
+    private double error;
+    private double power;
+    private double KP;
+    private double KI;
+    private double KD;
+    private double previousError = 0;
+    private double maxI;
+    private double previousTime = 0;
+
+
+    public PIDController(double KP, double KI, double KD, double maxI)
+    {
+        this.KP = KP;
+        this.KI = KI;
+        this.KD = KD;
+        this.maxI = maxI;
+    }
+
+    public double power(double target, double currentLoc)
+    {
+        double error = target - currentLoc;
+        double deltaTime = (System.nanoTime() - previousTime)/NANOSECS_PER_MIN;
+        i += Math.abs(currentLoc) > Math.abs(target) * 0.7 ? error*deltaTime : 0;
+        d = (error - previousError)/deltaTime;
+        previousTime = System.nanoTime();
+        previousError = error;
+        return power;
+    }
+
+    public double getI() {
+        return i;
+    }
+
+    public double getKP() {
+      return KP;
+    }
+
+    public double getKI() {
+        return KI;
+    }
+
+    public double getKD() {
+        return KD;
+    }
+
+
+    public void setKP(double KP){
+        this.KP = KP;
+    }
+
+    public void setKI(double KI){
+        this.KI = KI;
+    }
+
+    public void setKD(double KD){
+        this.KD = KD;
+    }
+
+    public void reset() {
+        i = 0;
+        previousError = 0;
+    }
+
+    public double[] returnVal() {
+        double PID[] = {(KP*error),(KI*i),(KD*d),power};
+        return PID;
+    }
+
+
+
+
+
+
+
+}
