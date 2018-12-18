32202196c3bcfd8f76bf3e3bb14a5d91ad763fdf
tarund2302 <tarund2302@gmail.com>
2018-12-17 22:05:57 -0500

    program for setting servo positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ServoPositioning.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Hardware.Hardware;
+
+@TeleOp(name = "ServoTesting")
+public class ServoPositioning extends OpMode{
+
+    private Hardware robot = new Hardware();
+    public Servo servo;
+    @Override
+    public void init() {
+        servo = hardwareMap.servo.get("servo");
+        servo.setPosition(0);
+    }
+
+    @Override
+    public void loop() {
+        double currPosition = servo.getPosition();
+
+        if(gamepad1.dpad_right){
+            servo.setPosition(currPosition + .1);
+        }
+        else if(gamepad1.dpad_left){
+            servo.setPosition(currPosition - .1);
+        }
+
+        if(gamepad1.dpad_up){
+            servo.setPosition(currPosition + .01);
+        }
+        else if(gamepad1.dpad_down){
+            servo.setPosition(currPosition - .01);
+        }
+        telemetry.addData("Servo position:", servo.getPosition());
+    }
+}
